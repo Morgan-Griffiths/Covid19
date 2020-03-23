f62b2e7581f83a29b8f325631628b6c99ea0faff
Morgan <C5ipo7i@gmail.com>
2020-03-23 14:26:14 -0700

    feat: added bash script for downloading datasets. added notebook for data analysis

---
+++ download_datasets.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+kaggle datasets download sudalairajkumar/novel-corona-virus-2019-dataset
+kaggle datasets download sudalairajkumar/covid19-in-italy
+kaggle datasets download sudalairajkumar/covid19-in-usa
+kaggle datasets download kimjihoo/coronavirusdataset
+
+$(rm -rf covid19-in-usa)
+$(rm -rf covid19-in-italy)
+$(rm -rf novel-corona-virus-2019-dataset)
+$(rm -rf coronavirusdataset)
+$(rm states.csv)
+$(rm time_series.csv)
+
+wget http://coronavirusapi.com/states.csv
+wget http://coronavirusapi.com/time_series.csv
+
+unzip novel-corona-virus-2019-dataset -d novel-corona-virus-2019-dataset
+unzip covid19-in-italy -d covid19-in-italy
+unzip covid19-in-usa -d covid19-in-usa
+unzip coronavirusdataset -d coronavirusdataset
+
+rm covid19-in-usa.zip
+rm covid19-in-italy.zip
+rm novel-corona-virus-2019-dataset.zip
+rm coronavirusdataset.zip
